0f7aee5e787992458d2c9ba1bcba0cd862fe0a69
Gagun45 <selyanchyn45@gmail.com>
2026-08-04 18:03:50 +0300

    homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import HomepageSection from "./_components/homepage-sections/HomepageSection";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <header className="bg-red-400 flex">Header</header>
+      <HomepageSection />
+      <footer className="bg-amber-400 flex mt-auto">Footer</footer>
+    </>
+  );
 }
