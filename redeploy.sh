6ce2fd94f5861d1a1a8fc4daf1eb965dcee9755c
HaaaamBurger <gemasclashes@gmail.com>
2026-08-14 18:50:57 +0300

    updating Dockerfile, redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -4,26 +4,31 @@ set -e
 APP_NAME="vendo-fe"
 CONTAINER_NAME="vendo-fe-app"
 PORT=3100
-API_URL="http://194.163.130.14:8030"
 
 echo "📥 Pull latest changes from Git..."
 git pull origin main
 
+echo "📄 Load environment variables..."
+set -a
+source .env
+set +a
+
 echo "🐳 Build Docker image..."
 docker build \
   --no-cache \
-  --build-arg NEXT_PUBLIC_API_URL=$API_URL \
-  -t $APP_NAME \
+  --build-arg NEXT_PUBLIC_API_URL="$NEXT_PUBLIC_API_URL" \
+  --build-arg NEXT_PUBLIC_GOOGLE_CLIENT_ID="$NEXT_PUBLIC_GOOGLE_CLIENT_ID" \
+  -t "$APP_NAME" \
   -f ./Dockerfile .
 
 echo "🛑 Stop old container (if exists)..."
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
+docker stop "$CONTAINER_NAME" || true
+docker rm "$CONTAINER_NAME" || true
 
 echo "🚀 Run new container..."
 docker run -d \
-  --name $CONTAINER_NAME \
-  -p $PORT:3000 \
-  $APP_NAME
+  --name "$CONTAINER_NAME" \
+  -p "$PORT:3000" \
+  "$APP_NAME"
 
 echo "✅ Deployed successfully on port $PORT"
